f0f79d4cd9acdb60cceccd0b5fd41ba0acc59013
kishan panchal <136459426+kishan-pravinbhai-panchal@users.noreply.github.com>
2023-12-16 21:14:34 +0530

    Add files via upload

---
+++ tic tac toe.py
@@ -0,0 +1,49 @@
+def print_board(board):
+    for row in board:
+        print(" | ".join(row))
+        print("-" * 9)
+
+def check_winner(board, player):
+    for row in board:
+        if all(cell == player for cell in row):
+            return True
+
+    for col in range(3):
+        if all(board[row][col] == player for row in range(3)):
+            return True
+
+    if all(board[i][i] == player for i in range(3)) or all(board[i][2 - i] == player for i in range(3)):
+        return True
+
+    return False
+
+def is_board_full(board):
+    return all(cell != ' ' for row in board for cell in row)
+
+def tic_tac_toe():
+    board = [[' ' for _ in range(3)] for _ in range(3)]
+    players = ['X', 'O']
+    turn = 0
+
+    while True:
+        print_board(board)
+        current_player = players[turn % 2]
+        print(f"Player {current_player}'s turn.")
+        row = int(input("Enter row number (0, 1, or 2): "))
+        col = int(input("Enter column number (0, 1, or 2): "))
+
+        if board[row][col] == ' ':
+            board[row][col] = current_player
+            if check_winner(board, current_player):
+                print_board(board)
+                print(f"Player {current_player} wins!")
+                break
+            elif is_board_full(board):
+                print_board(board)
+                print("It's a tie!")
+                break
+            turn += 1
+        else:
+            print("That position is already taken. Try again.")
+
+tic_tac_toe()
